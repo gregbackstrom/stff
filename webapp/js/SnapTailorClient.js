1fbe2e2ee450a020943b28adfd568b72a8f834e2
gregbackstrom <greg@snaptailor.com>
2014-09-01 13:48:01 -0700

    Added get profiles

--- webapp/js/SnapTailorClient.js
+++ webapp/js/SnapTailorClient.js
@@ -19,17 +19,37 @@ function saveFitProfile() {
 		myStuff.shoulderType = "square";
 		myStuff.shoulderLength = 19;
 		ff.createObjAtUri(myStuff, "/STFitProfile",
-	    function (data, statusMessage) {
+			function (data, statusMessage) {
 	        // successfully created object
-	        console.log('Bro, success!: ' + statusMessage);
+	        console.log('Bro, success!: ' + statusMessage + ', ' + data.createdBy);
 	    },
 	    function (statusCode, statusMessage) {
 	        // error occurred
 	        console.log('Bro, errror!: ' + statusCode + ', ' + statusMessage);
 	    });	
 	}, function(code, msg) {
-      console.error("Error logging in: " + code + ", error message" + msg);
-    });
+		console.error("Error logging in: " + code + ", error message" + msg);
+	});
 
 	
+}
+
+function readFitProfile() {
+	ff.login("test_user", "T3st_Us3r", function() {
+		console.log("LOGGED IN");
+		var query = "/STFitProfile/(shoulderType eq 'square')";
+		ff.getArrayFromUri(query,
+			function (objs) {
+				var fitProfile = objs;
+				console.log("Successful read");
+			}, function (statusCode, statusMessage) {
+        // error occurred
+        console.log("Error, broski:" + statusCode + ", " + statusMessage);
+    });
+		
+}, function(code, msg) {
+	console.error("Error logging in: " + code + ", error message" + msg);
+});
+
+
 }
